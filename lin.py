e818297352cb9c8a52d27a83f4c0c66ff3279594
PuneethRajY <142897797+PuneethRajY@users.noreply.github.com>
2023-08-24 16:34:38 +0530

    Update lin.py

--- lin.py
+++ lin.py
@@ -1,1 +1,14 @@
-
+import matplotlib.pyplot as plt
+import numpy as np
+#coordinate of first point
+x1 = 1
+y1=2
+#coordinate of second point
+x2=5
+y2=9
+#combine all x value and y value separately 
+x_values = [x1,x2]
+y_values = [y1,y2]
+#now plot x_values and y_values
+plt.plot(x_values,y_values)
+plt.show() 
